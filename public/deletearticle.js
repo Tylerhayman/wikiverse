85217025597a310c213c548b235c252387868f4e
Tylerhayman <128461465+Tylerhayman@users.noreply.github.com>
2023-05-19 14:49:52 +0100

    Basic form for deletion

---
+++ public/deletearticle.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import ReactDOM from 'react-dom/client';
+import {View, TextInput} from 'react-native';
+
+
+function deleteArticle() {
+  <form> 
+<label for="delete">Choose an article to delete:</label>
+
+<select name="articles" id="articles">
+  <option value="article1">placeholderarticle1</option>
+  <option value="article2">pha2</option>
+  <option value="article3">pha3</option>
+  <option value="article4">pha4</option>
+</select>
+
+<label for="authorEmail">Enter the Author's email.</label>
+<input value={email} type="authorEmail" placeholder="email@email.com" id="email" name="email"></input>
+
+<button> Submit</button>
+  </form>  
+}
